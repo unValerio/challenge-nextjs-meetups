3f9e3ddaf00229545e469b5fe14671c7a525a602
Valerio <sergioenrique@me.com>
2022-06-04 15:07:02 +0200

    Estructura para API

--- src/pages/all-meetups/AllMeetupsPage.js
+++ src/pages/all-meetups/AllMeetupsPage.js
@@ -4,6 +4,16 @@ import MeetupItem from './MeetupItem';
 import { MeetupsContainer } from './AllMeetupsPage.styled';
 
 export default function AllMeetups() {
+  const meetup = {
+    id: 'm1',
+    title: 'This is a first meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+    address: 'Meetupstreet 5, 12345 Meetup City',
+    description:
+      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+  };
+
   return (
     <>
       <Head>
@@ -12,10 +22,7 @@ export default function AllMeetups() {
       <section>
         <h1>All Meetups</h1>
         <MeetupsContainer>
-          <MeetupItem />
-          <MeetupItem />
-          <MeetupItem />
-          <MeetupItem />
+          <MeetupItem meetup={meetup} />
         </MeetupsContainer>
       </section>
     </>
--- src/pages/all-meetups/MeetupItem/MeetupItem.js
+++ src/pages/all-meetups/MeetupItem/MeetupItem.js
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 
-import { useFetch } from '../../../utils/hooks/useFetch';
 import {
   ItemWrapper,
   Card,
@@ -9,30 +8,23 @@ import {
   Actions,
 } from './MeetupItem.styled';
 
-export default function MeetupItem() {
-  const { data } = useFetch({
-    url: '/data.json',
-  });
-
-  if (!data) return <p>Loading...</p>;
-  let [item] = data;
-
+export default function MeetupItem({ meetup }) {
   return (
     <ItemWrapper>
       <Card>
         <ImageWrapper>
           <Image
-            src={item.image}
-            alt={item.title}
+            src={meetup.image}
+            alt={meetup.title}
             layout="fill"
             objectFit="cover"
             priority={50}
           />
         </ImageWrapper>
         <Content>
-          <h3>{item.title}</h3>
-          <address>{item.address}</address>
-          <p>{item.description}</p>
+          <h3>{meetup.title}</h3>
+          <address>{meetup.address}</address>
+          <p>{meetup.description}</p>
         </Content>
         <Actions>
           <button>Add to favorites</button>
